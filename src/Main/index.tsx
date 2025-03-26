11eaf31fe17f7d59bfbb223374e7bb73d5158aee
Matheus Soares <matheus.soares.msd@gmail.com>
2025-03-26 18:26:56 -0300

    feat: custom button and footer

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -8,6 +8,7 @@ import {
 import { Header } from '../components/Header';
 import { Categories } from '../components/Categories';
 import { Menu } from '../components/Menu';
+import { Button } from '../components/Button';
 
 export function Main() {
   return (
@@ -22,7 +23,9 @@ export function Main() {
         </MenuContainer>
       </Container>
       <Footer>
-        <FooterContainer></FooterContainer>
+        <FooterContainer>
+          <Button onPress={() => alert('New order')}>New Order</Button>
+        </FooterContainer>
       </Footer>
     </>
   );
