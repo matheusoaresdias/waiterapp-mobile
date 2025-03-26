dc8a32c4296f5875ae454961876e53731eceb40f
Matheus Soares <matheus.soares.msd@gmail.com>
2025-03-26 18:26:29 -0300

    feat: select category

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -2,8 +2,16 @@ import { FlatList } from 'react-native';
 import { categories } from '../../mocks/categories';
 import { Text } from '../Text';
 import { Category, Icon } from './styles';
+import { useState } from 'react';
 
 export function Categories() {
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  function handleSelectCategory(categoryId: string) {
+    const category = selectedCategory === categoryId ? '' : categoryId;
+    setSelectedCategory(category);
+  }
+
   return (
     <>
       <FlatList
@@ -12,25 +20,20 @@ export function Categories() {
         contentContainerStyle={{ paddingRight: 24 }}
         data={categories}
         keyExtractor={category => category._id}
-        renderItem={({ item: category }) => (
-          <Category>
-            <Icon>
-              <Text>{category.icon}</Text>
-            </Icon>
+        renderItem={({ item: category }) => {
+          const isSelected = selectedCategory === category._id;
 
-            <Text size={14} weight={600}>{category.name}</Text>
-          </Category>
-        )}
+          return (
+            <Category onPress={() => handleSelectCategory(category._id)}>
+              <Icon>
+                <Text opacity={isSelected ? 1 : 0.5}>{category.icon}</Text>
+              </Icon>
+
+              <Text size={14} weight={600} opacity={isSelected ? 1 : 0.5}>{category.name}</Text>
+            </Category>
+          )
+        }}
       />
-      {/*categories.map(category => (
-        <Category key={category._id}>
-          <Icon>
-            <Text>{category.icon}</Text>
-          </Icon>
-
-          <Text size={14} weight={600}>{category.name}</Text>
-        </Category>
-      ))*/}
     </>
   );
 }
