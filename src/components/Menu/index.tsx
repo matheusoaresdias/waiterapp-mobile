2736f324c28d8e2a88a1007aa264c31e05798a54
Matheus Soares <matheus.soares.msd@gmail.com>
2025-03-31 22:55:18 -0300

    feat: Menu layout and expo-constants

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,24 +1,39 @@
+import Constants from 'expo-constants';
 import { FlatList } from 'react-native';
 
 import { products } from '../../mocks/products';
 import { Text } from '../Text';
 
-import { Product, Image, ProductDetails } from './styles';
+import { Product, Image, Details, Separator } from './styles';
+import { formatCurrency } from '../../utils/formatCurrency';
 
 export function Menu() {
   return (
     <FlatList
       data={products}
+      style={{ marginTop: 32 }}
+      contentContainerStyle={{ paddingHorizontal: 24 }}
       keyExtractor={product => product._id}
+      ItemSeparatorComponent={Separator}
       renderItem={({ item: product }) => (
         <Product>
-          <Image />
+          <Image
+            source={{
+              uri: `${Constants.expoConfig?.extra?.IMAGE_URI}/${product.imagePath}`,
+            }}
+          />
 
-          <ProductDetails>
-            <Text>{product.name}</Text>
-            <Text>{product.description}</Text>
-            <Text>{product.price}</Text>
-          </ProductDetails>
+          <Details>
+            <Text weight={600}>{product.name}</Text>
+            <Text
+              size={14}
+              color="#666"
+              style={{ marginVertical: 8 }}
+            >
+              {product.description}
+            </Text>
+            <Text size={14} weight={600} color="#666">{formatCurrency(product.price)}</Text>
+          </Details>
         </Product>
       )}
     />
